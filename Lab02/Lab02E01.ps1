a99119766fff396fb27db9336a025c533314ba22
Simon Ozturk <simon@ozturks.net>
2021-09-07 23:31:01 -0700

    Lab 02 E01 Updated

--- Lab02/Lab02E01.ps1
+++ Lab02/Lab02E01.ps1
@@ -2,8 +2,34 @@
 using namespace Microsoft.SqlServer.Management.Smo
 [System.Reflection.Assembly]::LoadWithPartialname('Microsoft.SQLServer.SMO')
 
+#LAB 2
 
+#TASK 1
 $Instance = "MIA-SQL"   
 $User1 = "AdventureWorks\User1"
 $Server = [Server]::new($Instance)
 $Server.ConnectionContext.ExecuteWithResults("CREATE DATABASE AWDB")
+
+#TASK 2
+$Login = [Login]::new($Instance,$User1)
+$Login.LoginType = "WindowsUser"
+$Login.Create()
+$Login.AddToRole("dbcreator")
+
+#TASK 3
+$SQLLogin1 = "SQLLogin1"
+$Password = 'Pa55w.rd'
+$LoginSQL = [Login]::new($Instance,$SQLLogin1)
+$LoginSQL.LoginType = "SQLLogin"
+$LoginSQL.Create($Password)
+$AWDB = $Server.Databases["AWDB"]
+$DBUser = [User]::new($AWDB, $SQLLogin1)
+$DBUser.Login = $SQLLogin1
+$DBUser.DefaultSchema = "dbo"
+$DBUser.Create()
+$db_ddladmin = $AWDB.roles["db_ddladmin"]
+$db_ddladmin.AddMember($SQLLogin1)
+$db_datawriter = $AWDB.roles["db_datawriter"]
+$db_datawriter.AddMember($SQLLogin1)
+
+
